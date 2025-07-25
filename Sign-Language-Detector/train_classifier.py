6b031adf17e339431fe9379344a5873e8b70d273
Zaahir Sharma <sharma.zaahir@gmail.com>
2025-07-24 23:30:24 -0500

    Classifier test score and saving

--- Sign-Language-Detector/train_classifier.py
+++ Sign-Language-Detector/train_classifier.py
@@ -19,7 +19,7 @@ labels = np.asarray(data_dict['labels'])
 # Keeping 20% of the data for testing
 # x = data, y = labels
 # Keep same proportion of labels in both training and testing sets
-x_train, _x_test, y_train, _y_train = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 
 # Create model
@@ -29,4 +29,16 @@ model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
 # Predict on the test set
-y_predict = model.predict(_x_test)
+y_predict = model.predict(x_test)
+
+# Check accuracy of the model
+score = accuracy_score(y_predict, y_test)
+
+# Print the accuracy of the model, output was 100% when run
+print(f'Accuracy of the model: {score * 100:.2f}%')
+
+# Save the model to a pickle file
+f = open('model.pickle', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
+
